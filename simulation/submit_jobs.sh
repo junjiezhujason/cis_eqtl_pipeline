83bf64c2850c3302ce77d4935155e72a75856590
Jason Zhu <junjie.zhu.jason@gmail.com>
2017-02-26 13:18:02 -0800

    simulation script added for sherlock jobs

---
+++ simulation/submit_jobs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TRUESIG=20
+DIR=/scratch/PI/sabatti/controlled_access_data/simulation_data/high_dim_test_${TRUESIG}
+
+mkdir -p ${DIR}
+
+for i in {1..100}
+do
+    echo
+    echo $i
+    sbatch single_job.sbatch $i ${TRUESIG} ${DIR}
+done
