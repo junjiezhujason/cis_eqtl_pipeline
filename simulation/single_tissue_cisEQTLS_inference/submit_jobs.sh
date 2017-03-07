87bb134c1a5e324625b5b145452c981304dc6c36
Jason Zhu <junjie.zhu.jason@gmail.com>
2017-03-07 02:49:32 -0800

    added intelligent ways to decide on what batch jobs to submit

--- simulation/single_tissue_cisEQTLS_inference/submit_jobs.sh
+++ simulation/single_tissue_cisEQTLS_inference/submit_jobs.sh
@@ -3,9 +3,24 @@
 DIR=/scratch/PI/sabatti/controlled_access_data/cisEQTLS_sim_exp2
 SEED=1
 
-for i in {1..200}
+# for i in {1..200}
+# for i in {4001..4500}
+
+#for i in {4001..4500}
+#do
+#    echo 
+#    echo "GENE: $i"
+#    sbatch hi_hi_dim.sbatch $SEED $i $DIR single
+#    sbatch hi_hi_dim.sbatch $SEED $i $DIR double
+#    # bash hi_hi_dim.sbatch $SEED $i $DIR single
+#    # bash hi_hi_dim.sbatch $SEED $i $DIR double
+#done
+
+#for i in {3001..3500}
+for i in {2001..2500}
 do
     echo
+    echo "GENE: $i"
     sbatch hi_hi_dim.sbatch $SEED $i $DIR single
     sbatch hi_hi_dim.sbatch $SEED $i $DIR double
     # bash hi_hi_dim.sbatch $SEED $i $DIR single
---
+++ simulation/single_tissue_cisEQTLS_results/submit_jobs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sbatch evaluate_results.sbatch 0 500 single
+sbatch evaluate_results.sbatch 0 500 double
+
+sbatch evaluate_results.sbatch 2000 2500 single
+sbatch evaluate_results.sbatch 2000 2500 double
+
+sbatch evaluate_results.sbatch 3000 3500 single
+sbatch evaluate_results.sbatch 3000 3500 double
+
+sbatch evaluate_results.sbatch 4000 4500 single
+sbatch evaluate_results.sbatch 4000 4500 double
