55c35517a3e083bcdd57090d10e71e334a07ce67
Jason Zhu <junjie.zhu.jason@gmail.com>
2019-03-14 08:53:56 -0700

    removed old simulations

--- old/simulation/single_tissue_cisEQTLS/submit_Y_jobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=/scratch/PI/sabatti/controlled_access_data/cisEQTLS_sim_exp2
-for i in {1..10}
-do
-    echo
-    sbatch sim_Y_data.sbatch $i $DIR
-    # bash sim_Y_data.sbatch $i $DIR
-done
